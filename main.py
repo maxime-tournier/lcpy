304511298c83c9818eeb81792258dffa822ebd1a
Maxime Tournier <maxime.tournier@brain.riken.jp>
2014-11-21 00:14:13 +0900

    not much

--- main.py
+++ main.py
@@ -10,12 +10,16 @@ import metric
 import accel
 
 def parse_args():
+    """command line arguments"""
     import argparse
 
-    parser = argparse.ArgumentParser(description='benchmark LCP solvers.')
+    parser = argparse.ArgumentParser(description='benchmark LCP solvers')
+    
     parser.add_argument('file', nargs='+', help='filename for LCP data')
+
     parser.add_argument('--iter', type=int, default=100,
                         help='iteration count')
+    
     parser.add_argument('--fontsize', type=int, default=8,
                         help='font size in plot')
     
@@ -23,13 +27,12 @@ def parse_args():
                         help='disable legend in plots')
     
     parser.add_argument('--eps', type=float, default=1e-8,
-                   help='precision')
-
+                        help='precision')
+    
     return parser.parse_args()
 
 
 
-# some adapters
 def pgs(x, (M, q)):
     """PGS"""
     return splitting.pgs(x, M, -q)
@@ -38,15 +41,27 @@ def pjacobi(x, (M, q)):
     """PJacobi"""
     return splitting.pjacobi(x, M, -q)
 
+
+def solvers( (M, q) ):
+    """solver list"""
+    
+    return [pgs,
+            accel.nlnscg(pgs),
+            # accel.nlnscg(pgs, metric = np.diag(M) ),
+            accel.nlnscg(pgs, metric = np.diag(M), omega = 1.4 ),
+        ]
+
+
+
 args = parse_args()
 
-# params
+# solve params
 iterations = args.iter
 precision = args.eps
 
+# plot setup
 cols = min(3, len(args.file) )
 rows = int(math.ceil(len(args.file) / float(cols)))
-
 
 import matplotlib
 from matplotlib import pyplot as plt
@@ -61,48 +76,43 @@ for param in [ 'axes.titlesize',
 _, plots = plt.subplots(rows, cols)
 
 
-def solvers( (M, q) ):
-    """solver list"""
-    
-    return [pgs,
-            accel.nlnscg(pgs),
-            # accel.nlnscg(pgs, metric = np.diag(M) ),
-            accel.nlnscg(pgs, metric = np.diag(M), omega = 1.4 ),
-        ]
-
-
 
 for i, f in enumerate(args.file):
     
     (M, q) = lcp.load( f )
     
     # error metric 
-    error = metric.minimum_norm( (M, q)  )
+    error = metric.minimum_norm( (M, q) )
 
     print 'file:', f
     print 'dim:', q.size
     print 'metric:', error.__doc__
 
     p = plots[ i / cols, i % cols ] if rows > 1 else plots[ i ] if cols > 1 else plots
+
     
     for s in solvers( (M, q) ):
 
         name = s.__doc__
+
+        print '\t{}...'.format(name), 
 
         run = lcp.bench( (M, q), s,
                          iterations = iterations,
                          precision = precision,
                          metric = error )
 
-        print '\t{}...'.format(name), 
         data = [e for k, e in run]
+        
         print '\tit: {} \t eps: {:.2e}'.format(len(data), data[-1])
 
+        # plot
         p.plot( data, label = name )
         p.set_title('{} (n = {})'.format(f, q.size))
         p.set_yscale('log')
         if not args.nolegend: p.legend()
 
+        
 plt.show()
 
 
